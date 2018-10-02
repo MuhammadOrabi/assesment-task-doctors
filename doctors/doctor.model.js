073013af057f1435cb7ab19e6988df39e36fb608
Orabi <morabi@quadrantme.com>
2018-10-02 12:42:02 +0200

    update doctor schema

--- doctors/doctor.model.js
+++ doctors/doctor.model.js
@@ -5,7 +5,7 @@ const schema = new Schema({
     username: { type: String, index: true, unique: true, required: true },
     hash: { type: String, required: true },
     name: { type: String, required: true },
-    type: { type: String, required: true },
+    major: { type: String, required: true },
     appointment_time: { type: String, required: true },
     work: [{day: String, from: String, to: String}],
     createdDate: { type: Date, default: Date.now }
