17e665457a6ab42fc1a98f8e5124f07f7f4ffe82
Muhammad Orabi <morabi@quadrantme.com>
2018-10-22 17:00:59 +0200

    add rabbitmq support

--- server.js
+++ server.js
@@ -16,12 +16,14 @@ app.use(jwt());
 // api routes
 app.use('/api/doctors', require('./doctors/doctors.controller'));
 
+require('./doctors/doctor.rabbitmq');
+
 // global error handler
 app.use(errorHandler);
 
 // start server
 const port = process.env.PORT ? process.env.PORT : 3000;
-if (!module.parent) { 
+if (!module.parent) {
     const server = app.listen(port, function () {
         console.log('Server listening on port ' + port);
     });
