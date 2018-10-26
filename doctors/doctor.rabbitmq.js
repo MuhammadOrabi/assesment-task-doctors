23a8fcb9fa9167bba5d83a2b3134f17a4aff77bd
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2018-10-26 22:26:04 +0200

    update get all/get avail with rabbitmq

--- doctors/doctor.rabbitmq.js
+++ doctors/doctor.rabbitmq.js
@@ -1,5 +1,6 @@
+const axios = require('axios');
+const _ = require('underscore');
 const rabbitmq = require('_helpers/rabbitmq');
-
 const doctorService = require('./doctor.service');
 
 rabbitmq.getFromMQ('front', 'doctor.create', msg => {
@@ -29,6 +30,39 @@ rabbitmq.getFromMQ('front', 'doctor.authenticate', msg => {
     })
     .catch(err => {
         rabbitmq.sendToMQ('doctor.authenticate.error', {err: err});
+        throw err;
+    });
+});
+
+rabbitmq.getFromMQ('front', 'doctor.get.all', msg => {
+    console.log("[doctor] %s",'doctor.get.all');
+    doctorService.getAll()
+    .then(doctors => {
+        if (doctors) {
+            rabbitmq.sendToMQ('doctor.got.all', doctors);            
+        } else {
+            rabbitmq.sendToMQ('doctor.get.all.error', {err: 'something went wrong!'});            
+        }
+    })
+    .catch(err => {
+        rabbitmq.sendToMQ('doctor.get.all.error', {err: err});
+        throw err;
+    });
+});
+
+rabbitmq.getFromMQ('front', 'doctor.get.avail.day', async msg => {
+    let params = JSON.parse(msg.content.toString());
+    console.log("[doctor] %s",'doctor.get.avail.day');
+    doctorService.getByIdAndAvail(params.id, params.day)
+    .then(doctor => {
+        if (doctor) {
+            rabbitmq.sendToMQ('doctor.got.avail.day', doctor);            
+        } else {
+            rabbitmq.sendToMQ('doctor.get.avail.day.error', {err: 'something went wrong!'});            
+        }
+    })
+    .catch(err => {
+        rabbitmq.sendToMQ('doctor.get.avail.day.error', {err: err});
         throw err;
     });
 });
